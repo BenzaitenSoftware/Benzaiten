dbd3fdd07f604e81ee3658ebc0c2f303452a9858
LukeJansen <lpjansen@live.co.uk>
2019-10-30 12:18:47 +0000

    Date from Server Start
    - Started working on Date from Server code

---
+++ Benzaiten Language Game/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TimeManager : MonoBehaviour
+{
+    private string url = "localhost:8000/time.php";
+    private string currentTime, currentDate;
+
+    private void Start()
+    {
+        StartCoroutine(GetTimeFromServer());
+    }
+
+    IEnumerator GetTimeFromServer()
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(webRequest.error);
+            }
+            else
+            {
+                Debug.Log(webRequest.downloadHandler.text);
+            }
+        }
+    }
+}
